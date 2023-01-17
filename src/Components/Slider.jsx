2f722c89bd31eb48880cf90e6eca5692321fd241
Anas Abdullah <anas.abdullah815@gmail.com>
2023-01-18 02:51:24 +0500

    Slider completed

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined'
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined'
+import { useState } from "react"
+import { SliderItems } from "../data"
 
 const Container = styled.div`
 width: 80%;
@@ -17,6 +19,8 @@ const Wrapper = styled.div`
 height: 100%;
 //width: inherit;
 display: flex;
+transition: all 1.5s ease;
+transform: translateX(${props => props.slideIndex * -100}vw);
 `;
 
 const Arrow = styled.div`
@@ -33,7 +37,11 @@ bottom: 0;
 margin: auto;
 cursor: pointer;
 opacity: 0.5;
-
+z-index: 2;
+&:hover{
+    background-color: white;
+    zoom: 1.05;
+}
 //using props
 // If the value of the direction prop is "left" we will set the left value to be 10px;
 // If the value of the direction prop is "right" we will set the right value to be 10px;
@@ -78,7 +86,7 @@ height: 80%;
 `; */
 const Image = styled.img`
 height: 80%;
-width: 100%;
+width: 78%;
 `;
 
 /* const Title = styled.h1`
@@ -91,9 +99,21 @@ const Button = styled.button``; */
 
 
 const Slider = () => {
-  return (
+  
+    const [slideIndex, setSlideIndex] = useState(0); 
+    //function to implement arrow button functionality
+    const handleClick = (direction) => {
+        if(direction === "left"){
+            setSlideIndex(slideIndex > 0 ?  slideIndex - 1: SliderItems.length - 1); //if slide index > 0 decrement slide index by 1
+            // else go to the last slide       
+        }
+        else{
+            setSlideIndex(slideIndex < SliderItems.length - 1 ? slideIndex + 1: 0);
+        }
+    }
+    return (
     <Container>
-        <Arrow direction = "left" /*passing props to our styled component. direction is a prop. we are passing the value "left to it" */ >  
+        <Arrow direction = "left" onClick={()=>handleClick("left")} /*passing props to our styled component. direction is a prop. we are passing the value "left to it" */ >  
             <ArrowLeftOutlinedIcon/>
         </Arrow>
        {/*  <Wrapper>
@@ -103,24 +123,16 @@ const Slider = () => {
                 </ImgContainer>
             </Slide>
         </Wrapper> */}
-        <Wrapper>
+        <Wrapper slideIndex = {slideIndex}>
+            {SliderItems.map((item) =>(
           <Slide>
             <ImgContainer>
-                  <Image src="https://images.unsplash.com/photo-1563910060166-e1ef7463a9aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+                  <Image src={item.img} />
               </ImgContainer>
           </Slide>
-          <Slide>
-            <ImgContainer>
-                  <Image src="https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" />
-              </ImgContainer>
-          </Slide>
-          <Slide>
-              <ImgContainer>
-                  <Image src="https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" />
-              </ImgContainer>
-          </Slide>
+          ))}
           </Wrapper>
-          <Arrow direction="right">
+          <Arrow direction="right" onClick={()=>handleClick("right")}>
               <ArrowRightOutlinedIcon />
           </Arrow>  
     </Container>

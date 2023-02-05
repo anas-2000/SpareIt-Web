fbc5ee9327867a9b22b097811d9ece4524cbc273
Anas Abdullah <anas.abdullah815@gmail.com>
2023-02-05 17:14:11 +0500

    shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import LogIn from "./Pages/LogIn";
 import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
 import SignUp from "./Pages/SignUp";
+import ShoppingCart from "./Pages/ShoppingCart"
 
 
 
 const App = () => {
    //return <Home/>;
    //return <Product />
-   return <SignUp />
+   //return <SignUp />
    //return <LogIn />
+   return <ShoppingCart />
 };
 
 export default App;

3b0229c97f4e161fa5d7a2b44898b23a4b2742b3
Anas Abdullah <anas.abdullah815@gmail.com>
2023-02-05 12:53:42 +0500

    SignUp page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
+import SignUp from "./Pages/SignUp";
+
 
 
 const App = () => {
    //return <Home/>;
-   return <Product />
+   //return <Product />
+   return <SignUp />
 };
 
 export default App;

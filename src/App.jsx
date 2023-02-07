3eb5290f0feb15ae5cb8b3a1b9ef2e5faa1aa5a9
Anas Abdullah <anas.abdullah815@gmail.com>
2023-02-08 00:04:41 +0500

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,46 @@ import ProductList from "./Pages/ProductList";
 import SignUp from "./Pages/SignUp";
 import ShoppingCart from "./Pages/ShoppingCart"
 import Checkout from "./Pages/Checkout";
+//import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 
 const App = () => {
-   return <Home />;
+   const user = false; //to be deleted later. Just for demo
+   
+   return (
+      <BrowserRouter>
+         <Routes>
+            {/* <Route exact path="/">
+               <Home />
+            </Route> */}
+            <Route exact path="/" element={<Home />} />
+            <Route path="/products/:category" element={<ProductList />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* <Route path="/products/:category">
+               <ProductList />
+            </Route>
+            <Route path="/product/:id">
+               <Product />
+            </Route>
+            <Route path="/cart">
+               <ShoppingCart />
+            </Route>
+            <Route path="checkout">
+               <Checkout />
+            </Route>
+            <Route path="/login">{user ? <Navigate to="/" /> : <LogIn />}</Route>
+            <Route path="/register">
+               {user ? <Navigate to="/" /> : <SignUp />}
+            </Route> */}
+         </Routes>
+      </BrowserRouter>
+   );
+   // return <Home />;
    //return <Product />
    //return <SignUp />
    //return <LogIn />

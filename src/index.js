cdffbefbce9082a292a40ff4f6c509b2930ff41e
Anas Abdullah <anas.abdullah815@gmail.com>
2023-03-19 12:11:52 +0500

    integration done. Integration of Stripe left

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './Redux/store'
+//import store from './Redux/store'
 // import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux/es';
+import { store, persistor } from "./Redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   // <React.StrictMode>
